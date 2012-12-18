da94bfcd318ecf1be20d5444a06319417d36c01e
kyle-ung <kayan.leung@gettyimages.com>
2012-12-18 14:11:13 -0700

    Revert "committing sandwich app"
    This reverts commit 51389aaa722f017440aea81f11ca66b2a04c9cf0.

--- railsinstaller_demo/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RailsinstallerDemo::Application.config.secret_token = '3e5b5f5a6a3357ba0aa2ab7a7d05ff84f016250fda921b368c23befd539c9c734c079f68e47feeb7806726d1ad0d2bb756a522c25f8764b11a11eb52c7ca2f4e'
