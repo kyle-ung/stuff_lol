e54c40040b637d02598dc8263c2cc38381939a14
kyle-ung <kayan.leung@gettyimages.com>
2013-01-16 10:48:39 -0700

    Blogger code -- with bug - le sigh

---
+++ projects/blogger/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '134d1bda53ae03e0fecc73685fe72bba4cb0fc7870954507c8b3abf816c4385ffe50d524c2cfe3eb515cb64622f8066ef97ea26f1d9e4c21bf1a621dded81cee'
